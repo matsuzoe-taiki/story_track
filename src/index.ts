0d3471d67f7c186deca9cd29f985936b6a05a475
matsuzoe-taiki <matsutaiyaki1013@yahoo.co.jp>
2026-07-28 18:54:04 +0900

    テーブルに行が追加される処理を追加しました。（テスト）

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 const addRowButton: HTMLElement | null = document.getElementById('addrowbutton');
 
-if (addRowButton !== null) {
-    addRowButton.addEventListener("click", () => {
-        console.log('こんにちは');
-    });
-}
+const addRow = () => {
+    const COLUMN_COUNT = 6
+
+    const tableBody: HTMLElement | null = document.getElementById('tablebody');
+    const tableRow: HTMLElement = document.createElement('tr');
+
+    for (let count = 0; count < COLUMN_COUNT; count++) {
+        const tableData: HTMLElement = document.createElement('td');
+
+        tableData.textContent = 'hello';
+
+        tableRow.appendChild(tableData);
+    }
+
+    tableBody!.appendChild(tableRow);
+};
+
+addRowButton!.addEventListener("click", addRow);
