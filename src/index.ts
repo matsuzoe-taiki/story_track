41035563ac2a4dbe6c157824491b0274f68f98a4
matsuzoe-taiki <matsutaiyaki1013@yahoo.co.jp>
2026-07-31 10:52:43 +0900

    追加を押すと行を作成し、各カラムのinputタグが作成される機能を作成しました

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,49 @@
 const addRowButton: HTMLElement | null = document.getElementById('addrowbutton');
+const tableBody: HTMLElement | null = document.getElementById('tablebody');
 
 const addRow = () => {
-    const COLUMN_COUNT = 6
+    const COLUMN_COUNT: number = 6;
 
-    const tableBody: HTMLElement | null = document.getElementById('tablebody');
     const tableRow: HTMLElement = document.createElement('tr');
 
     for (let count = 0; count < COLUMN_COUNT; count++) {
         const tableData: HTMLElement = document.createElement('td');
+        const columnInput: HTMLElement = document.createElement('input');
 
-        tableData.textContent = 'hello';
+        switch (count) {
+            case 0:
+                columnInput.setAttribute('type', 'checkbox');
 
+                tableData.appendChild(columnInput);
+                break;
+            case 1:
+                tableData.appendChild(columnInput);
+                break;
+            case 2:
+                columnInput.setAttribute('style', 'width: 100px')
+                columnInput.setAttribute('type', 'number');
+
+                tableData.appendChild(columnInput);
+                break;
+            case 3:
+                columnInput.setAttribute('style', 'width: 100px')
+                columnInput.setAttribute('type', 'number');
+
+                tableData.appendChild(columnInput);
+                break;
+            case 4:
+                columnInput.setAttribute('type', 'date');
+
+                tableData.appendChild(columnInput);
+                break;
+            case 5:
+                columnInput.setAttribute('type', 'date');
+
+                tableData.appendChild(columnInput);
+                break;
+        }
         tableRow.appendChild(tableData);
     }
-
     tableBody!.appendChild(tableRow);
 };
 
