ca487fb094dee46dca7b63a5941218e4dea860fa
matsuzoe-taiki <matsutaiyaki1013@yahoo.co.jp>
2026-08-01 13:01:32 +0900

    全てのinputにidを追加する処理を作成

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,13 @@ function onAddRow(): void {
     const START_DATE_COLUMN: number = 4;
     const LAST_WATCH_DATE_COLUMN: number = 5;
 
+    const rowId: string = crypto.randomUUID();
+
     const tableRow: HTMLElement = document.createElement('tr');
 
-    createRecord();
+    tableRow.setAttribute('data-id', rowId);
+
+    createRecord(rowId);
 
     for (let columnIndex = 0; columnIndex < COLUMN_COUNT; columnIndex++) {
         const tableData: HTMLElement = document.createElement('td');
@@ -21,27 +25,35 @@ function onAddRow(): void {
         switch (columnIndex) {
             case END_CHECK_COLUMN:
                 columnInput.setAttribute('type', 'checkbox');
+                columnInput.setAttribute('data-id', rowId);
                 tableData.appendChild(columnInput);
+
+                columnInput.addEventListener("input", onEndCheckChange)
                 break;
             case TITLE_COLUMN:
                 tableData.appendChild(columnInput);
+                columnInput.setAttribute('data-id', rowId);
                 break;
             case SEASON_COLUMN:
                 columnInput.setAttribute('style', 'width: 100px')
                 columnInput.setAttribute('type', 'number');
+                columnInput.setAttribute('data-id', rowId);
                 tableData.appendChild(columnInput);
                 break;
             case EPISODE_COLUMN:
                 columnInput.setAttribute('style', 'width: 100px')
                 columnInput.setAttribute('type', 'number');
+                columnInput.setAttribute('data-id', rowId);
                 tableData.appendChild(columnInput);
                 break;
             case START_DATE_COLUMN:
                 columnInput.setAttribute('type', 'date');
+                columnInput.setAttribute('data-id', rowId);
                 tableData.appendChild(columnInput);
                 break;
             case LAST_WATCH_DATE_COLUMN:
                 columnInput.setAttribute('type', 'date');
+                columnInput.setAttribute('data-id', rowId);
                 tableData.appendChild(columnInput);
                 break;
         }
@@ -50,9 +62,13 @@ function onAddRow(): void {
     tableBody!.appendChild(tableRow);
 };
 
+function onEndCheckChange() {
+
+}
+
 addRowButton!.addEventListener("click", onAddRow);
 
-function createRecord(): void {
+function createRecord(recordId: string): void {
     type AnimeRecord = {
         id: string,
         isCompleted: boolean,
@@ -65,7 +81,7 @@ function createRecord(): void {
 
     const newRecord: AnimeRecord =
     {
-        id: crypto.randomUUID(),
+        id: recordId,
         isCompleted: false,
         title: '',
         season: 0,
